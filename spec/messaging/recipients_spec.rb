7bf8435eea8a4bce2fe3c2acd6cc9211b51ee5f4
amerdidit <amer@amerdid.it>
2014-12-09 17:48:48 +0100

    Add Dyn::Messaging::Recipients#statuses
    - Add callback method to handle multiple recipient status requests.
    - Add examples
    - Add specs

--- spec/messaging/recipients_spec.rb
+++ spec/messaging/recipients_spec.rb
@@ -2,28 +2,39 @@ require 'spec_helper'
 
 describe Dyn::Messaging::Client do
 
-  describe "recipients()" do
-    emailaddress = "abc@foobar.com"
+  describe 'recipients()' do
+    email_address = 'abc@foobar.com'
+    email_address_2 = 'another@foobar.com'
+    email_addresses_string = email_address + ', ' + email_address_2
+    email_addresses_array = [email_address, email_address_2]
 
     subject { @dyn.recipients }
 
     it "should retrieve the status of an email address" do
 
-      stub = stub_request(:get, "#{@API_BASE_PATH}/recipients/status?apikey=#{@DEFAULT_API_KEY}&emailaddress=#{emailaddress}")
+      stub = stub_request(:get, "#{@API_BASE_PATH}/recipients/status?apikey=#{@DEFAULT_API_KEY}&emailaddress=#{email_address}")
 
-      subject.send(:status, emailaddress)
+      subject.send(:status, email_address)
 
       expect(stub).to have_been_requested
     end
 
-    it "should activate an email address" do
-      stub = stub_request(:post, "#{@API_BASE_PATH}/recipients/activate")
-      .with(:body => {"apikey"=>"#{@DEFAULT_API_KEY}", "emailaddress"=>emailaddress})
+    it 'should retrieve the status of an list of email addresses' do
 
-      subject.send(:activate, emailaddress)
+      stub = stub_request(:get, "#{@API_BASE_PATH}/recipients/status?apikey=#{@DEFAULT_API_KEY}&emailaddress=#{email_addresses_string}")
+
+      subject.send(:statuses, email_addresses_array)
+
+      expect(stub).to have_been_requested
+    end
+
+    it 'should activate an email address' do
+      stub = stub_request(:post, "#{@API_BASE_PATH}/recipients/activate")
+             .with(body: { 'apikey' => "#{@DEFAULT_API_KEY}", 'emailaddress' => email_address })
+
+      subject.send(:activate, email_address)
 
       expect(stub).to have_been_requested
     end
   end
-
 end
